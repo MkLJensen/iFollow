c4c519dd8358775be5f3a80da76259d96d3c92b9
Mathias <Mathias@4juhl.dk>
2019-04-10 10:21:04 +0200

    Uno

---
+++ Software/ArduinoUno/Sensor_Test_1/Sensor_Test_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Sensor_Test_1.c
+ *
+ * Created: 10-04-2019 10:18:48
+ * Author : Mathias
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
