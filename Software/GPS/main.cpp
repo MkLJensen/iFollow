aebff8af0f1608892a278c99bb1359424b69d326
Your Name <you@example.com>
2019-05-01 15:24:38 +0200

    GPS mySQL imple

--- Software/GPS/main.cpp
+++ Software/GPS/main.cpp
@@ -1,24 +1,66 @@
 #include "getGPS.hpp"
 #include <iostream>
 
+//SQL Includes
+
+
+#include <mysql_connection.h>
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+#define DBHOST "tcp://10.192.139.5:3306"
+#define USER "Jesus"
+#define PASSWORD "pi"
+#define DATABASE "DatabaseGPS"
 
 
 int main(void)
 {
     GPS myGPS("/dev/ttyAMA0");
 
+    //SQL Declerations
+    sql::Driver *driver;
+    sql::Connection *conn;
+    sql::Statement *stmt;
+    sql::ResultSet *res;
+
+    /*Create a Connection*/
+    driver = get_driver_instance();
+    conn = driver->connect(DBHOST,USER,PASSWORD);
+
+    /*Connecting to mySQL database */
+    conn->setSchema(DATABASE);
+
+    char executeQueryChar[1000];
+
+    myGPS.updateCordinates();
+
+    
+    int n = sprintf(executeQueryChar,"INSERT INTO `GPSData`(`nmeaType`, `fixTime`, `Latitude`, `Longitude`, `fixQuality`, `numOfSats`, `horizDilofPos`, `Altitude`, `heightofGeoID`, `checkSum`) VALUES (`%s`,`%s`,`%s`,`%s`,`%s`,`%s`,`%s`,`%s`,`%s`,`%s`);",
+        myGPS.getGpsType().c_str(),
+        myGPS.getfixTime().c_str(),
+        myGPS.getLatitude().c_str(),
+        myGPS.getLongitude().c_str(),
+        myGPS.getfixQuality().c_str(),
+        myGPS.getnumOfSats().c_str(),
+        myGPS.gethDilutionofPos().c_str(),
+        myGPS.getaltitudeMeters().c_str(),
+        myGPS.gethOgGeoid().c_str(),
+        myGPS.getcheckSum().c_str()
+        );
+
+    std::string executeQueryString = executeQueryChar;
+    
+    stmt = conn->createStatement();
+    res = stmt->executeQuery(executeQueryString);
 
 
-        myGPS.updateCordinates();
+    delete res;
+    delete stmt;
+    delete conn;
 
-        std::cout << "GPSType: " << myGPS.getGpsType() << std::endl;
-        std::cout << "fixTime: " << myGPS.getfixTime() << std::endl;
-        std::cout << " Lat: " << myGPS.getLatitude() << " Long: " << myGPS.getLongitude() << std::endl;
-        std::cout << "fixQuality: " << myGPS.getfixQuality() << std::endl;
-        std::cout << "Number of Satelites: " << myGPS.getnumOfSats() << std::endl;
-        std::cout << "hDilutionofPos: " << myGPS.gethDilutionofPos() << std::endl;
-        std::cout << "Altitude in Meters: " << myGPS.getaltitudeMeters() << std::endl;
-        std::cout << "Geo ID: " << myGPS.gethOgGeoid() << std::endl;
-        std::cout << "checkSum: " << myGPS.getcheckSum() << std::endl;
     return 0;
 }
