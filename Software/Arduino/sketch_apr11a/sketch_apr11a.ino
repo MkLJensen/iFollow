8dabd020df351aefdf0747e8e169a27d8de6107f
Mathias <Mathias@4juhl.dk>
2019-05-14 13:19:04 +0200

    Arduino

--- Software/Arduino/sketch_apr11a/sketch_apr11a.ino
+++ Software/Arduino/sketch_apr11a/sketch_apr11a.ino
@@ -1,4 +1,8 @@
+#include "Adafruit_VL53L0X.h"
 #include "SPI.h"
+
+//Array of range data to send to PSoC
+uint8_t sensorData[7] = {0,0,0,0,0,0,0};
 
 // SPI slave-select pin
 const int slavePin = 53;
@@ -6,18 +10,28 @@ const int slavePin = 53;
 //Speed, data order and data mode - to be matched with PSoC creator settings
 SPISettings MasterSettings(2000000, MSBFIRST, SPI_MODE0);
 
+// address we will assign if dual sensor is present
+#define LOX1_ADDRESS 0x30
+#define LOX2_ADDRESS 0x31
+#define LOX3_ADDRESS 0x32
 
-/*
-    Reset all sensors by setting all of their XSHUT pins low for delay(10), then set all XSHUT high to bring out of reset
-    Keep sensor #1 awake by keeping XSHUT pin high
-    Put all other sensors into shutdown by pulling XSHUT pins low
-    Initialize sensor #1 with lox.begin(new_i2c_address) Pick any number but 0x29 and it must be under 0x7F. Going with 0x30 to 0x3F is probably OK.
-    Keep sensor #1 awake, and now bring sensor #2 out of reset by setting its XSHUT pin high.
-    Initialize sensor #2 with lox.begin(new_i2c_address) Pick any number but 0x29 and whatever you set the first sensor to
-<<<<<<< HEAD
-*/
-=======
- */
+// set the pins to shutdown
+#define SHT_LOX1 7
+#define SHT_LOX2 6
+#define SHT_LOX3 5
+
+// objects for the vl53l0x
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+Adafruit_VL53L0X lox3 = Adafruit_VL53L0X();
+
+// this holds the measurement
+VL53L0X_RangingMeasurementData_t measure1;
+VL53L0X_RangingMeasurementData_t measure2;
+VL53L0X_RangingMeasurementData_t measure3;
+
+
+
 void setID() {
   // all reset
   digitalWrite(SHT_LOX1, LOW);    
@@ -114,43 +128,6 @@ void read_three_ranges(){
   Serial.println();
 }
 
-void read_dual_sensors() {
-  
-  lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
-  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
-  lox3.rangingTest(&measure3, false); // pass in 'true' to get debug data printout!
-
-  // print sensor one reading
-  Serial.print("1: ");
-  if(measure1.RangeStatus != 4) {     // if not out of range
-    Serial.print(measure1.RangeMilliMeter);
-  } else {
-    Serial.print("Out of range");
-  }
-  
-  Serial.print(" ");
-
-  // print sensor two reading
-  Serial.print("2: ");
-  if(measure2.RangeStatus != 4) {
-    Serial.print(measure2.RangeMilliMeter);
-  } else {
-    Serial.print("Out of range");
-  }
-
-  Serial.print(" ");
-  
-  // print sensor three reading
-  Serial.print("3: ");
-  if(measure3.RangeStatus != 4) {
-    Serial.print(measure3.RangeMilliMeter);
-  } else {
-    Serial.print("Out of range");
-  }
-  
-  Serial.println();
-}
-
 void setup() {
   //SS pin set to output
   pinMode (slavePin, OUTPUT);
@@ -167,12 +144,8 @@ void setup() {
 }
 
 void loop() { 
-  // read_dual_sensors();
-  read_three_ranges();
-  delay(100);
-
   read_three_ranges(); // Update global array sensorData
-  SPI.beginTransaction(slaveSettings);
+  SPI.beginTransaction(MasterSettings);
   digitalWrite (slavePin, LOW);
   SPI.transfer(sensorData[0]); // Start byte
   delay(3);
